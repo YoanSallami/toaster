30d0a50811bf8262bfd5bccb83732a00654a3f34
Yoan Sallami <ysallami@aoide.laas.fr>
2016-11-07 16:34:49 +0100

    modified:   area_manager/src/main.cpp

--- area_manager/src/main.cpp
+++ area_manager/src/main.cpp
@@ -510,7 +510,7 @@ int main(int argc, char** argv) {
         // Humans
         for (std::map<std::string, Human*>::iterator it = humanRd.lastConfig_.begin(); it != humanRd.lastConfig_.end(); ++it) {
             // We update area with human center
-            for(std::map<std::string, Joint*>::iterator it2 = it->second->skeleton_.begin() , it2 != it->second->skeleton_.end(); ++it2)
+            for(std::map<std::string, Joint*>::iterator it2 = it->second->skeleton_.begin() ; it2 != it->second->skeleton_.end() ; ++it2)
 			    mapEntities_[it2->first]=it2-second;
             mapEntities_[it->first] = it->second;
             updateEntityArea(mapArea_, it->second);
